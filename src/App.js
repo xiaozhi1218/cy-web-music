36498a7186f240e77af0909a0f8a2e4f2c1ebde9
chenyongzhi <chenyongzhijjk@163.com>
2021-03-05 10:54:54 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { memo } from 'react';
+
+import { renderRoutes } from 'react-router-config';
+
+import routes from './router';
+
+import HYAppHeader from '@/components/app-header';
+import HYAppFooter from '@/components/app-footer';
+import { HashRouter } from 'react-router-dom';
+
+export default memo(function App() {
+  return (
+    <div>
+      <HashRouter>
+        <HYAppHeader/>
+        {renderRoutes(routes)}
+        <HYAppFooter/>
+      </HashRouter>
+    </div>
+  )
+})
---
+++ src/pages/player/index.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+export default memo(function HYPlayer() {
+    return (
+        <div>
+            <h2>Player</h2>
+        </div>
+    )
+})
