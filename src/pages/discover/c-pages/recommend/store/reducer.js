e0482e3b3943aae9b1d976f2200a0ae7d4663c92
chenyongzhi <chenyongzhijjk@163.com>
2021-03-10 20:23:18 +0800

    immutable引入

--- src/pages/discover/c-pages/recommend/store/reducer.js
+++ src/pages/discover/c-pages/recommend/store/reducer.js
@@ -1,18 +1,8 @@
-// import { Map } from 'immutable';
+import { Map } from 'immutable';
 
 import * as actionTypes from './constants';
 
-// const defaultState = Map({
-//   topBanners: [],
-//   hotRecommends: [],
-//   newAlbums: [],
-
-//   upRanking: {},
-//   newRanking: {},
-//   originRanking: {},
-// });
-
-const defaultState = {
+const defaultState = Map({
   topBanners: [],
   hotRecommends: [],
   newAlbums: [],
@@ -20,28 +10,38 @@ const defaultState = {
   upRanking: {},
   newRanking: {},
   originRanking: {},
-};
+});
+
+// const defaultState = {
+//   topBanners: [],
+//   hotRecommends: [],
+//   newAlbums: [],
+
+//   upRanking: {},
+//   newRanking: {},
+//   originRanking: {},
+// };
 
 function reducer(state = defaultState, action) {
   switch (action.type) {
     case actionTypes.CHANGE_TOP_BANNERS:
-      // return state.set("topBanners", action.topBanners);
-      return {...state, topBanners: action.topBanners};
+      return state.set("topBanners", action.topBanners);
+      // return {...state, topBanners: action.topBanners};
     case actionTypes.CHANGE_HOT_RECOMMEND:
-      // return state.set("hotRecommends", action.hotRecommends);
-      return {...state, hotRecommends: action.hotRecommends};
+      return state.set("hotRecommends", action.hotRecommends);
+      // return {...state, hotRecommends: action.hotRecommends};
     case actionTypes.CHANGE_NEW_ALBUM:
-      // return state.set("newAlbums", action.newAlbums);
-      return {...state, newAlbums: action.newAlbums};
+      return state.set("newAlbums", action.newAlbums);
+      // return {...state, newAlbums: action.newAlbums};
     case actionTypes.CHANGE_UP_RANKING:
-      // return state.set("upRanking", action.upRanking);
-      return {...state, upRanking: action.upRanking};
+      return state.set("upRanking", action.upRanking);
+      // return {...state, upRanking: action.upRanking};
     case actionTypes.CHANGE_NEW_RANKING:
-      // return state.set("newRanking", action.newRanking);
-      return {...state, newRanking: action.newRanking};
+      return state.set("newRanking", action.newRanking);
+      // return {...state, newRanking: action.newRanking};
     case actionTypes.CHANGE_ORIGIN_RANKING:
-      // return state.set("originRanking", action.originRanking);
-      return {...state, originRanking: action.originRanking};
+      return state.set("originRanking", action.originRanking);
+      // return {...state, originRanking: action.originRanking};
     default:
       return state;
   }
